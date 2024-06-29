54ad8cd9e5d4b5535b9f804c20af3906ea6b562e
asionsolver <asionsolver274@gmail.com>
2024-06-29 18:38:03 +0600

    implement RegistrationForm using react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { LoginForm } from './components/forms/LoginForm';
+// import { LoginForm } from './components/forms/LoginForm';
+
+import { RegistrationForm } from './components/forms/RegistrationForm';
 
 function App() {
   return (
     <>
       <h1 className=" text-center text-3xl font-bold ">React Hook Form Crash Course</h1>
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <RegistrationForm />
     </>
   );
 }
