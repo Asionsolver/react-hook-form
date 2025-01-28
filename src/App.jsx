9ba6398c8343f5f4df1a2c7bf0653fa09fea1db0
asionsolver <asionsolver274@gmail.com>
2025-01-28 11:51:44 +0600

    learn basic react hook form and how to handing error in react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 // import { LoginForm } from './components/forms/LoginForm';
 
-import { RegistrationForm } from './components/forms/RegistrationForm';
+import SimpleForm from "./components/forms/simple/SimpleForm";
+
+// import { RegistrationForm } from "./components/forms/RegistrationForm";
 
 function App() {
   return (
     <>
-      <h1 className=" text-center text-3xl font-bold ">React Hook Form Crash Course</h1>
+      <h1 className=" text-center text-3xl font-bold ">
+        React Hook Form Crash Course
+      </h1>
       {/* <LoginForm /> */}
-      <RegistrationForm />
+
+      {/* <RegistrationForm /> */}
+      {/* <Easy /> */}
+      <SimpleForm />
     </>
   );
 }
---
+++ src/components/forms/simple/Easy.jsx
@@ -0,0 +1,46 @@
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+export const Easy = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    if (data.name) {
+      toast.success(` Your name is ${data.name}`, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+  return (
+    <div>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            type="text"
+            id="name"
+            {...register("name")}
+          />
+        </div>
+        <button
+          type="submit"
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Submit
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
